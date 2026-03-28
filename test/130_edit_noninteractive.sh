7fb56bf47cf7ce66ff83b969424c41a4be9a0cf3
Alexey Naidyonov <alexey.naidyonov@gmail.com>
2026-03-28 23:26:43 +0100

    fix: integration test

--- test/130_edit_noninteractive.sh
+++ test/130_edit_noninteractive.sh
@@ -41,15 +41,15 @@ EOINVALID
 EOF
 chmod 0755 "${CASE_DIR}/write-invalid-env.sh"
 
-run_case_expect_ok "edit_env" edit --edit ./write-env.sh env
+run_case_expect_ok "edit_env" edit --editor ./write-env.sh env
 assert_manifest_contains "EDITOR_VALUE = \"from-editor\""
 
-run_case_expect_ok "edit_script" edit --edit ./write-script.sh script
+run_case_expect_ok "edit_script" edit --editor ./write-script.sh script
 assert_manifest_contains "/opt/edit/script.txt"
 
 run_case_expect_ok \
     "edit_artifact" \
-    edit --edit ./write-artifact.sh artifact notice.txt --target /etc/edit/notice.txt --stage
+    edit --editor ./write-artifact.sh artifact notice.txt --target /etc/edit/notice.txt --stage
 assert_manifest_contains "[artifact.\"notice.txt\"]"
 assert_manifest_contains "/etc/edit/notice.txt"
 
@@ -59,7 +59,7 @@ assert_runtime_file_content "${TREE_DIR}" /etc/edit/notice.txt "edited notice"
 
 cp -- "${MANIFEST}" "${CASE_DIR}/Manifest.before-invalid.toml"
 cp -- "${LOCK}" "${CASE_DIR}/Manifest.before-invalid.lock"
-run_case_expect_fail "edit_env_invalid" edit --edit ./write-invalid-env.sh env
+run_case_expect_fail "edit_env_invalid" edit --editor ./write-invalid-env.sh env
 assert_stderr_contains "invalid env line"
 cmp -s "${MANIFEST}" "${CASE_DIR}/Manifest.before-invalid.toml" || die "manifest rollback failed"
 cmp -s "${LOCK}" "${CASE_DIR}/Manifest.before-invalid.lock" || die "lock rollback failed"
